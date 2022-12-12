1ab7cbbdcb16b1a79c4c25488cc5cd6cc890705a
Jagath <jagathsingularity@gmail.com>
2022-12-12 16:27:17 +0530

    encapsulation assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        RWOnly obj = new RWOnly("Yin");
+
+        // Task 4 :
+        // obj.name; // Compile Error: not a statement
+        // Not able to access name variable as it's private
+
+        // Task 6:
+        obj.setName("Yang");
+
+        System.out.println(obj.getName()); // Yang
+
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name;
+
+    RWOnly(String name){
+        this.name = name;
+    }
+
+    // Task 5 : setter and getter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
